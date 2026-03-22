62e29fba27f56ce8febe536fd1afcd57ec15a569
jinishgupta <24je0629@iitism.ac.in>
2026-03-23 01:39:23 +0530

    changed urls after deploying

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,11 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173', 
+      'http://127.0.0.1:5173',
+      'https://quizard-gljo.onrender.com'
+    ],
     methods: ['GET', 'POST'],
     credentials: true,
   },
